e23b7cc816073e8978efe7168ee6fa643ee4c244
dcc0 <ivangavr777@gmail.com>
2019-03-21 08:35:38 +0300

    Update cross_object.js

--- dia/lib/js/cross_object.js
+++ dia/lib/js/cross_object.js
@@ -1,5 +1,6 @@
 <script>
-		// Кроссбраузерное создание объекта дл запроса . Обновление чата
+// Кроссбраузерное создание объекта дл запроса . Обновление чата
+
 		function getXmlHttp(){
 		    var xmlhttp;
 		    try {
@@ -20,42 +21,49 @@
 		    return xmlhttp;
 		}
 
+
 		function update() {
 
 		    var xmlhttp = getXmlHttp()
-        	//Запрос
+ //Запрос
 		    xmlhttp.open('GET', "/dia/lib/view/Class_Printing.php", true);
 
 		    xmlhttp.onreadystatechange = function() {
 		        if (xmlhttp.readyState == 4) {
 
 		            if(xmlhttp.status == 200) {
-						//Получение JSON
+//Получение JSON
 				var mydata=xmlhttp.responseText;
 				var itm, span_text;
+
+
 
         itm = document.getElementById("parent"); //Получаем id div-a
         span_text = document.createElement("span");  //Создадим span для текста
         span_text.innerHTML = '<span style="display:inline-block;  bottom: 200px; background-color: green; width: 10px; height: ' + mydata+'px;"></span>'; //Добавим текст и перенос строки
         itm.appendChild(span_text); //Добавим text к div
 
-		//Скролл прижмём
+//Скролл прижмём
                 document.getElementById('updatebox').scrollLeft = 9999999;
 		            }
 		        }
 		    };
     xmlhttp.send(null);
-		}	
-		//Каждые 30 минут обновим окно
+		}
+//Каждые 30 минут обновим окно
    window.setInterval('refresh()', 1800000000); 	// Call a function every 1800000000 milliseconds (OR 30 mins).
 
-                       // Refresh or reload page.
-                         function refresh() {
-                         window .location.reload();
-                      }
+// Refresh or reload page.
+        function refresh() {
+        window .location.reload();
+        }
+
+        function stopFunction(){
+		alert ('stop');
+		}
 
 		// Таймер
-		var time = 1;
-	setInterval("update()", time);
+		var time = 1000;
+		setInterval("update()", time);
 
 </script>
