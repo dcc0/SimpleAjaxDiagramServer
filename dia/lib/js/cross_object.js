26c5a57c01735d4cfadfec3718d393f4d1d4915a
dcc0 <ivangavr777@gmail.com>
2019-03-21 06:43:48 +0300

    Update cross_object.js

--- dia/lib/js/cross_object.js
+++ dia/lib/js/cross_object.js
@@ -1,8 +1,5 @@
-
 <script>
-
 		// Кроссбраузерное создание объекта дл запроса . Обновление чата
-
 		function getXmlHttp(){
 		    var xmlhttp;
 		    try {
@@ -23,7 +20,6 @@
 		    return xmlhttp;
 		}
 
-
 		function update() {
 
 		    var xmlhttp = getXmlHttp()
@@ -34,25 +30,23 @@
 		        if (xmlhttp.readyState == 4) {
 
 		            if(xmlhttp.status == 200) {
-
-		     //Получение JSON
-
-		            var mydata=xmlhttp.responseText;
-
-      var itm, span_text;
+						//Получение JSON
+				var mydata=xmlhttp.responseText;
+				var itm, span_text;
 
         itm = document.getElementById("parent"); //Получаем id div-a
         span_text = document.createElement("span");  //Создадим span для текста
         span_text.innerHTML = '<span style="display:inline-block;  bottom: 200px; background-color: green; width: 10px; height: ' + mydata+'px;"></span>'; //Добавим текст и перенос строки
         itm.appendChild(span_text); //Добавим text к div
 
+		//Скролл прижмём
+                document.getElementById('updatebox').scrollLeft = 9999999;
 		            }
 		        }
 		    };
     xmlhttp.send(null);
-
-		}
-	//Каждые 30 минут обновим окно
+		}	
+		//Каждые 30 минут обновим окно
    window.setInterval('refresh()', 1800000000); 	// Call a function every 1800000000 milliseconds (OR 30 mins).
 
                        // Refresh or reload page.
@@ -61,7 +55,7 @@
                       }
 
 		// Таймер
-		var time = 1000;
+		var time = 1;
 	setInterval("update()", time);
 
 </script>
